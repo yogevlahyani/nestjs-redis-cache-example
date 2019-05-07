695f86ca1d7c4dcbd70fd865f32b04957e9596d3
Yogev Lahyani <yogev.lahyani@autodesk.com>
2019-05-07 15:40:45 +0300

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import * as redisStore from 'cache-manager-redis-store';
+import { CacheModule, Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    CacheModule.register({
+      store: redisStore,
+      host: 'localhost',
+      port: 6379,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
